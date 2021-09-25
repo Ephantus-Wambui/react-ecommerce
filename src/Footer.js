5a7b5815aab6c39a560af3ef1e50ea20195c13fc
Ephantus Wambui <ephywambui72@gmail.com>
2021-09-25 15:45:59 +0300

    styled footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="footer">
+            <div className="footer-container">
+                <h3>E-commerce</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis veniam nobis alias perferendis ea asperiores consequatur.
+                </p>
+            </div>
+            <div className="footer-container">
+                <h3>Products</h3>
+                <p>Product 1</p>
+                <p>Product 2</p>
+                <p>Product 3</p>
+            </div>
+            <div className="footer-container">
+                <h3>Find us on</h3>
+                <p><a href="https://www.facebook.com/">Facebook</a></p>
+                <p><a href="https://www.instagram.com/">Instagram</a></p>
+                <p><a href="https://www.youtube.com/">Youtube</a></p>
+            </div>
         </div>
     );
 }
