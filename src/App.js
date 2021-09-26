d81ece0bfeb910e4161b38bfbfc61509704eca31
Ephantus Wambui <ephywambui72@gmail.com>
2021-09-26 08:51:45 +0300

    added navigation pages

--- src/App.js
+++ src/App.js
@@ -1,99 +1,14 @@
 import './App.css';
-import { Image } from 'react-bootstrap';
-import image1 from './image/galaxy-buds.jfif';
-import image2 from './image/hp-envy-360.jfif';
-import image3 from './image/iPhone-13.jfif';
-import image4 from './image/m1-macbook.jfif';
-import image5 from './image/mercedes-amg.jfif';
-import image6 from './image/surface-studio-3.jfif';
 import NavBar from './NavBar';
-import Rating from './Rating';
 import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Product />
       <Footer />
     </div>
   );
 }
 
 export default App;
-
-function Product() {
-  return (
-    <div className="products">
-      <div className="product-container">
-        <div className="container">
-          <Image className="photo" src={image1} fluid />
-          <p>Samsung galaxy buds</p>
-          <p>$ 99.99</p>
-          <p><Rating rating="4" /></p>
-          <a href="/">Buy</a>
-        </div>
-        <div className="container">
-          <Image className="photo" src={image2} fluid />
-          <p>hp Envy 360</p>
-          <p>$ 699.99</p>
-          <p><Rating rating="4" /></p>
-          <a href="/">Buy</a>
-        </div>
-        <div className="container">
-          <Image className="photo" src={image3} fluid />
-          <p>iPhone 13</p>
-          <p>$ 799.99</p>
-          <p><Rating rating="3" /></p>
-          <a href="/">Buy</a>
-        </div>
-      </div>
-      <div className="product-container">
-        <div className="container">
-          <Image className="photo" src={image4} fluid />
-          <p>M1 macbook</p>
-          <p>$ 1499.00</p>
-          <p><Rating rating="4" /></p>
-          <a href="/">Buy</a>
-        </div>
-        <div className="container">
-          <Image className="photo" src={image5} fluid />
-          <p>Mercedes AMG</p>
-          <p>$ 54750.99</p>
-          <p><Rating rating="5" /></p>
-          <a href="/">Buy</a>
-        </div>
-        <div className="container">
-          <Image className="photo" src={image6} fluid />
-          <p>Microsoft Surface Studio 3</p>
-          <p>$ 3499.99</p>
-          <p><Rating rating="4" /></p>
-          <a href="/">Buy</a>
-        </div>
-      </div>
-      <div className="product-container">
-        <div className="container">
-          <Image className="photo" src={image4} fluid />
-          <p>M1 macbook</p>
-          <p>$ 1499.00</p>
-          <p><Rating rating="4" /></p>
-          <a href="/">Buy</a>
-        </div>
-        <div className="container">
-          <Image className="photo" src={image5} fluid />
-          <p>Mercedes AMG</p>
-          <p>$ 54750.99</p>
-          <p><Rating rating="5" /></p>
-          <a href="/">Buy</a>
-        </div>
-        <div className="container">
-          <Image className="photo" src={image6} fluid />
-          <p>Microsoft Surface Studio 3</p>
-          <p>$ 3499.99</p>
-          <p><Rating rating="4" /></p>
-          <a href="/">Buy</a>
-        </div>
-      </div>
-    </div>
-  );
-}
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Reviews from './Reviews';
+import Checkout from './Checkout';
+import Contactus from './Contactus';
 
 function NavBar() {
   return (
+    <Router>
     <div>
-      <Nav fill variant="tabs" defaultActiveKey="/home">
+      <Nav fill variant="tabs">
         <Nav.Item>
-          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/">Home</Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link href="/reviews" eventKey="link-1">Reviews</Nav.Link>
@@ -15,12 +21,19 @@ function NavBar() {
           <Nav.Link href="/checkout" eventKey="link-2">Checkout</Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/contactUs" eventKey="link-3">
-            Contact Us
-          </Nav.Link>
+          <Nav.Link href="/contactUs" eventKey="link-3">Contact Us</Nav.Link>
         </Nav.Item>
       </Nav>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/reviews" component={Reviews} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/contactUs" component={Contactus} />
+        </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
