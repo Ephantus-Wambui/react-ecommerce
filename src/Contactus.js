ff85ba58c349ccd5b4063f98ce73291841a334a3
Ephantus Wambui <ephywambui72@gmail.com>
2021-09-26 12:22:43 +0300

    styled contact us page

--- src/Contactus.js
+++ src/Contactus.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 function Contactus() {
-    return (
-        <div>
-            <h1>Contact Us</h1>
-        </div>
-    );
+  return (
+    <div className="contact">
+
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicText">
+          <Form.Label>Fullname</Form.Label>
+          <Form.Control type="text" placeholder="Fullname" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicText">
+          <Form.Label>Subject</Form.Label>
+          <Form.Control type="text" placeholder="Subject" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicText">
+          <Form.Label>Message</Form.Label>
+          <Form.Control type="text-area" placeholder="Message" />
+        </Form.Group>
+        <Button variant="primary" className="mb-3" type="submit">
+          Submit
+        </Button>
+      </Form>
+
+    </div>
+  );
 }
 
 export default Contactus;
